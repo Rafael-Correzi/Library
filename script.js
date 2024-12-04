e6d0533e5a9b8e6df6d26a0965cdecb9dff4d326
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-12-04 19:22:59 -0300

    Fazer envio de formulário adicionar à tabela

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const table = document.querySelector(".livros");
 const lib = [];
 const tr = [];
 const td = [];
+let livro;
+
+const form = document.querySelector("#form-livro");
 
 function Book(titulo, autor, paginas, lido) {
   this.titulo = titulo;
   this.autor = autor;
   this.paginas = paginas;
-  this.lido = lido ? "Lido" : "Ainda não";
+  this.lido = lido ? "Lido" : "Não lido";
 }
 
 Book.prototype.addBookToLib = function (){
   lib.push(this);
+  console.log(this);
 }
 
-function traverseLib() {
-  for (i = 0; i < lib.length; i++){
+function traverseLib(tamanho = 0) {
+  //O parâmetro tamanho existe para quando a função for chamada no envio do formulário
+  for (i = tamanho; i < lib.length; i++){
     tr[i] = document.createElement("tr");
     table.appendChild(tr[i]);
     for (j = 0; j < 4; j++) {
@@ -29,6 +34,17 @@ function traverseLib() {
     td[3].textContent = lib[i].lido;
   }
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  livro = new Book(titulo.value,
+                   autor.value,
+                   paginas.value,
+                   +document.querySelector('input[name="estado"]:checked').value);
+  livro.addBookToLib();
+  traverseLib(lib.length-1)
+  //Porque só precisamos que a função traverseLib adicione uma única nova linha à tabela;
+} )
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
 theHobbit.addBookToLib();
