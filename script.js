b5861967420bc37d5640f1c4f12add221a2633ea
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-12-20 13:16:19 -0300

    Adicionar botões de troca de estado

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const table = document.querySelector(".livros");
 const lib = [];
 const tr = [];
 const td = [];
-const button = [];
+const buttonR = [];
+const buttonLido = [];
+
 
 let livro;
 
@@ -12,7 +14,7 @@ function Book(titulo, autor, paginas, lido) {
   this.titulo = titulo;
   this.autor = autor;
   this.paginas = paginas;
-  this.lido = lido ? "Lido" : "Não lido";
+  this.lido = lido;
 }
 
 Book.prototype.addBookToLib = function (){
@@ -24,24 +26,29 @@ function traverseLib(tamanho = 0) {
   //O parâmetro tamanho existe para quando a função for chamada no envio do formulário
   for (i = tamanho; i < lib.length; i++){
     tr[i] = document.createElement("tr");
-    button[i] = document.createElement("button");
-    button[i].setAttribute("type", "button");
-    button[i].setAttribute("class", "remover");
-    button[i].setAttribute("data-index", i);
-    button[i].textContent = "Remover Livro";
+    buttonR[i] = document.createElement("button");
+    buttonLido[i] = document.createElement("button");
+    buttonR[i].setAttribute("type", "button");
+    buttonLido[i].setAttribute("type", "button");
+    buttonR[i].textContent = "Remover";
     table.appendChild(tr[i]);
+    td[i] = [];
     for (j = 0; j < 4; j++) {
       //Porque cada livro tem quatro atributos
-      td[j] = document.createElement("td");
-      tr[i].appendChild(td[j]);
+      td[i][j] = document.createElement("td");
+      console.log(tr[i]);
+      console.log(td[i][j]);
+      tr[i].appendChild((td[i][j]));
     }
-    td[0].textContent = lib[i].titulo;
-    td[1].textContent = lib[i].autor;
-    td[2].textContent = lib[i].paginas;
-    td[3].textContent = lib[i].lido;
-    tr[i].appendChild(button[i]);
-    td.splice(0, 4);
+    td[i][0].textContent = lib[i].titulo;
+    td[i][1].textContent = lib[i].autor;
+    td[i][2].textContent = lib[i].paginas;
+    td[i][3].textContent = lib[i].lido ? "Lido" : "Não lido";
+    buttonLido[i].textContent = lib[i].lido ? "Trocar para não lido" : "Trocar para lido";
+    tr[i].appendChild(buttonLido[i]);
+    tr[i].appendChild(buttonR[i]);
     removerLivros(i);
+    trocarEstado(i);
   }
 }
 
@@ -60,14 +67,23 @@ const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
 theHobbit.addBookToLib();
 
 function removerLivros(indice) {
-  button[indice].addEventListener("click", () => {
+  buttonR[indice].addEventListener("click", () => {
     lib.splice(indice, 1);
-    button.splice(indice, 1);
+    buttonR.splice(indice, 1);
+    td.splice(indice, 1);
     tr[indice].remove();
     tr.splice(indice, 1);
   })
 }
 
+function trocarEstado(indice) {
+  buttonLido[indice].addEventListener("click", () => {
+    lib[indice].lido = lib[indice].lido ? 0 : 1;
+    td[indice][3].textContent = lib[indice].lido ? "Lido" : "Não lido";
+  })
+}
+
 
 traverseLib();
+
 
